4a6409ce30f79a875b841077565d880c4859564d
ISK <iskoroom@gmail.com>
2020-12-30 21:14:03 +0900

    change app text3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed!
+          Please WORK!!!!
         </a>
       </header>
     </div>
